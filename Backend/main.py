129cea38cf1244459a220d80e67e651781c8ebfe
teestakar <teestakar447@gmail.com>
2026-03-09 01:35:50 +0530

    Fix CORS to allow all origins

--- Backend/main.py
+++ Backend/main.py
@@ -183,8 +183,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://fireflux.netlify.app", "http://localhost:5500", "http://127.0.0.1:5500"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
